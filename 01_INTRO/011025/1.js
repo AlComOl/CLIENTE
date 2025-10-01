11f59efea7306f4e7905932f255ff4363709a264
AlComOl <alvcomoli@alu.edu.gva.es>
2025-10-01 09:47:14 +0200

    clase de hoy

---
+++ 01_INTRO/011025/1.js
@@ -0,0 +1,17 @@
+//github.io/styleguide/jsguide
+console.log("hola")
+//repasar bucles
+//numero primo
+
+let i=3;
+
+
+
+for (let i=0;i<5;++i){
+    console.log(i);
+}
+console.log(i);
+for (let i=0;i<5;i++){
+    console.log(i);
+}
+console.log("Hola, Node está funcionando");
