70a3daabf0f87532ce8eb1f6b3084a8a8c267af5
Alvaro <alvcomoli@alu.edu.gva.es>
2025-10-01 11:42:14 +0200

    primoterminado

---
+++ 01_INTRO/011025/primo.js
@@ -0,0 +1,19 @@
+let n=17;
+let flag=false;
+let cont=2;
+
+
+    
+while(n%cont==0&&cont<n){
+    cont++;
+    flag=true;
+}
+
+let primo= true ? console.log("No es primo") : console.log("Es primo");
+
+//  if(flag==true){
+//     console.log("No es primo")
+
+//  }else{
+//     console.log("Es primo")
+//  }
