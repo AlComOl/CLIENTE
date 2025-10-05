d039c71ff521109c5432cf46ed7512e85d046d42
Alvaro <alvcomoli@alu.edu.gva.es>
2025-10-05 09:19:23 +0200

    bd

--- 01_INTRO/31025/a.js
+++ 01_INTRO/31025/a.js
@@ -1,6 +1,6 @@
-const character = "#";
-const count = 8;
-const rows = [];
+// const character = "#";
+// const count = 8;
+// const rows = [];
 
 
 // for (let i = 0; i < count; i = i + 1) {
@@ -10,26 +10,81 @@ const rows = [];
 // console.log(rows);
 
 
-for (let i = 0; i < count; i++) {
-  rows.push(character.repeat(i+1)+ " \n");
+// for (let i = 0; i < count; i++) {
+//   rows.push(character.repeat(i+1)+ " \n");
   
-}
-console.log(rows);
+// }
+// console.log(rows);
+
+// function padRow(rowNumber, rowCount) {
+//   return character.repeat(rowNumber);
+// }
+
+// let f= padRow("hola","caracola");
+
+// console.log(padRow);
+
+
+// function padRow(rowNumber, rowCount) {
+//   return " "+character+" ".repeat(rowNumber);
+// }
+
+
+// function padRow(rowNumber, rowCount) {
+//   return " ".repeat(rowCount-rowNumber) + character.repeat(rowNumber) + " ".repeat(rowCount-rowNumber);
+// }
+
+
+// function padRow(rowNumber, rowCount) {
+//   return " ".repeat(rowCount - rowNumber) + character.repeat(2*rowNumber-1) + " ".repeat(rowCount - rowNumber);
+// }
+
+// // continueLoop=false;//Loop Infinito
+// // while (continueLoop) {
+// //  console.log("hola");
+// // }
+// let continueLoop = true;
+// let done = 0;
+
+// while (continueLoop) {
+//   done++;
+//   rows.push(padRow(done, count));
+//   if (done === count) {
+//     continueLoop = false;
+//   } 
+// }
+
+const character = "#";
+const count = 8;
+const rows = [];
+let continueLoop = true;
+let done = 0;
 
 function padRow(rowNumber, rowCount) {
-  return character.repeat(rowNumber);
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 
-let f= padRow("hola","caracola");
+// while (continueLoop) {
+//   done++;
+//   rows.push(padRow(done, count));
+//   if (done === count) {
+//     continueLoop = false;
+//   } 
+// }
 
-console.log(padRow);
-
-
-function padRow(rowNumber, rowCount) {
-  return " "+character+" ".repeat(rowNumber);
+while (done !== count) {
+  done++;
+  rows.push(padRow(done, count));
+  if (done === count) {
+    continueLoop = false;
+  } 
 }
 
 
-function padRow(rowNumber, rowCount) {
-  return " ".repeat(rowCount-rowNumber) + character.repeat(rowNumber) + " ".repeat(rowCount-rowNumber);
+let result = ""
+
+for (const row of rows) {
+  result = result + row + "\n";
 }
+
+console.log(result);
