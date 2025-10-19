eb4704a4e80d9c797d437aeeeb377eed4b016b15
Alvaro <alvcomoli@alu.edu.gva.es>
2025-10-19 22:04:06 +0200

    recursivas que no entinendo

---
+++ 01_INTRO/freeCodeCAm/recursividad/a1.js
@@ -0,0 +1,21 @@
+function countup(n) {
+  if (n < 1) {
+    return [];
+  } else {
+    const countArray = countup(n - 1);
+    countArray.push(n);
+    return countArray;
+  }
+}
+console.log(countup(5));
+
+
+
+
+function rangeOfNumbers(startNum, endNum) {
+  if (startNum === endNum) {
+    return [startNum];
+  } else {
+    return [startNum].concat(rangeOfNumbers(startNum + 1, endNum));
+  }
+}
