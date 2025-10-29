cf594284f9946d202d39600b1eca5456ae73b6a4
AlComOl <alvcomoli@alu.edu.gva.es>
2025-10-29 09:49:43 +0100

    clase js

---
+++ 01_INTRO/291025/a.js
@@ -0,0 +1,55 @@
+//UD4 OBJETOS PREDEFINIDOS JS
+
+//HTTP ONLY COOKIS SEGURAS
+
+//https://cipfpbatoi.github.io/materials/daw/dwc/01-js/04-DOM.html
+
+//https://www.youtube.com/watch?v=y17RuWkWdn8
+//DOM 
+//cuelga de window -> document
+
+///dos tipo de nodos 
+// const body =document.body
+// body.append("Hello world");
+
+// //const body =document.body
+// //body.appendChild("Hello world");
+
+
+// const div=document.createElement("div");
+
+// body.append(div);
+
+// div.innerText="Hello word";
+// div.textContent="Hello world2";
+
+
+// const div= document.querySelector("div");
+// console.log(div.textContent)
+
+
+
+// const body= document.body;
+// const div = document.createElement("dic")
+// const strong = document.createElement("strong")
+// div.append(strong);
+// body.append(div);
+
+
+const body=document.body
+const div= document.querySelector("div");
+const spanHi= document.querySelector("#h1")
+const spapnBye= document.querySelector("#bye")
+
+console.log(spanHi.setAttribute("title"),"");
+
+
+spanHi.DATA_SECTION_NODE.nawName= "Hi"
+
+spanHi.classList.toggle("")
+spanHi.style
+
+
+
+
+console.log(s)
