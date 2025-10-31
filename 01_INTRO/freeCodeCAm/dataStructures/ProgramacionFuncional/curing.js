58496f81699f954b77c84bd5d225f0b44a3d5635
Alvaro <alvcomoli@alu.edu.gva.es>
2025-10-31 23:30:52 +0100

    casa js

--- 01_INTRO/freeCodeCAm/dataStructures/ProgramacionFuncional/curing.js
+++ 01_INTRO/freeCodeCAm/dataStructures/ProgramacionFuncional/curing.js
@@ -1,7 +1,9 @@
-function add(x) {
-  return function(y) {
-    return function(z) {
-      return x + y + z;
+function add(x) {//llamas a la primera funcion y le pasas 10
+  return function(y) { //=> la primera funcion devuelve otra funcion , la que espera otro parametro
+    return function(z) {//=> la segunda funcion devuelve una tercera , la que espera otro parametro
+      return x + y + z;// => ahora esta tercera devuelve el resultado
     };
   };
 }
+
+console.log(add(10)(20)(30));//por eso hay que pasasr 3 argumentos
--- 01_INTRO/freeCodeCAm/dataStructures/numerosRomanos.js
+++ 01_INTRO/freeCodeCAm/dataStructures/numerosRomanos.js
@@ -1,19 +1,41 @@
 function Converter(numero){
-    let romano;
-    let phrase=String(numero);
-    const a=phrase.split(",");
-    
-    phrase.forEach(phrase,index)=>{
-    
-    });
+ function convertToRoman(numero) {
+    const romanNumeralConverter = {
+        M: 1000,
+        CM: 900,
+        D: 500,
+        CD: 400,
+        C: 100,
+        XC: 90,
+        L: 50,
+        XL: 40,
+        X: 10,
+        IX: 9,
+        V: 5,
+        IV: 4,
+        I: 1
+    };
 
-    console.log(element);
+    let resultado = "";
 
+    for (let simbolo in romanNumeralConverter) {
+        let valor = romanNumeralConverter[simbolo];
+        while (numero >= valor) {
+            resultado += simbolo;
+            numero -= valor;
+        }
+    }
 
-
-    return numero;
+    return resultado;
+}
 
 }
+
+
+
+
+
+
 
 
 console.log(Converter(1975));
