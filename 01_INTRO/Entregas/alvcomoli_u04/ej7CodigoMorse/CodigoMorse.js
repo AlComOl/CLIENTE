33db5126ee64e93da07cc82dcae4e229e13f1beb
AlComOl <alvcomoli@alu.edu.gva.es>
2025-11-10 13:02:40 +0100

    morse

--- 01_INTRO/Entregas/alvcomoli_u04/ej7CodigoMorse/CodigoMorse.js
+++ 01_INTRO/Entregas/alvcomoli_u04/ej7CodigoMorse/CodigoMorse.js
@@ -9,10 +9,25 @@ function codigoMorse(phrase){
         // let element = phraseArray[i];
         // element ==='.'|| element==='-' ? morse++ : abcedario++; //contador
 
-        if(phraseArray[i] === '.' || phraseArray[i] === '-') {
-            morse++;
-        } else {
-            abcedario++;
+        // if(phraseArray[i] === '.' || phraseArray[i] === '-') {//cambiar
+        //     morse++;
+        // } else {
+        //     abcedario++;
+        // }
+
+     
+if ((phraseArray[i].toLowerCase() >= 'a' && phraseArray[i].toLowerCase() <= 'z') ||  (phraseArray[i] >= '0' && phraseArray[i] <= '9'))
+
+        
+            
+
+            return aMorse(phraseArray);
+          }
+
+
+        }else{
+
+            return aEspañol(phraseArray);
         }
         
     }
@@ -25,7 +40,7 @@ function codigoMorse(phrase){
 
     }else if(morse>(phraseArray.length/2)+1){
 
-       return aEspañol(phrase);
+       return aEspañol(phraseArray);
     }
 
 }
@@ -33,7 +48,7 @@ function codigoMorse(phrase){
 function aMorse(phraseArray){
 
    const morse = {
-  "A": "· —",
+  "A": "·—",
   "B": "— · · ·",
   "C": "— · — ·",
   "CH": "— — — —",
@@ -89,7 +104,72 @@ function aMorse(phraseArray){
 }
 
 
+// function aEspañol(phraseArray){
 
+
+//   const morse = {
+//   "A": "· —",
+//   "B": "— · · ·",
+//   "C": "— · — ·",
+//   "CH": "— — — —",
+//   "D": "— · ·",
+//   "E": "·",
+//   "F": "· · — ·",
+//   "G": "— — ·",
+//   "H": "· · · ·",
+//   "I": "· ·",
+//   "J": "· — — —",
+//   "K": "— · —",
+//   "L": "· — · ·",
+//   "M": "— —",
+//   "N": "— ·",
+//   "Ñ": "— — · — —",
+//   "O": "— — —",
+//   "P": "· — — ·",
+//   "Q": "— — · —",
+//   "R": "· — ·",
+//   "S": "· · ·",
+//   "T": "—",
+//   "U": "· · —",
+//   "V": "· · · —",
+//   "W": "· — —",
+//   "X": "— · · —",
+//   "Y": "— · — —",
+//   "Z": "— — · ·",
+//   "0": "— — — — —",
+//   "1": "· — — — —",
+//   "2": "· · — — —",
+//   "3": "· · · — —",
+//   "4": "· · · · —",
+//   "5": "· · · · ·",
+//   "6": "— · · · ·",
+//   "7": "— — · · ·",
+//   "8": "— — — · ·",
+//   "9": "— — — — ·",
+//   ".": "· — · — · —",
+//   ",": "— — · · — —",
+//   "?": "· · — — · ·",
+//   "\"": "· — · · — ·",
+//   "/": "— · · — ·"
+// }
+
+// let arrayMorse=Object.entries(morse);
+
+// // let reverse=arrayMorse.map((letra)=>)
+
+// console.log(arrayMorse);
+
+
+
+// let newphrase=phraseArray.entries();
+
+// newphrase.map
+
+//  console.log(newphrase); 
+
+//  return arrayMorse;
+
+// }
 
 
 
