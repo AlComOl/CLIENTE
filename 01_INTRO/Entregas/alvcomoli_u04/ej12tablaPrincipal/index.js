a5e70e0a5f217a9e6d4d0e87fe348d6a01dde075
Alvaro <alvcomoli@alu.edu.gva.es>
2025-11-14 21:57:41 +0100

    12

--- 01_INTRO/Entregas/alvcomoli_u04/ej12tablaPrincipal/index.js
+++ 01_INTRO/Entregas/alvcomoli_u04/ej12tablaPrincipal/index.js
@@ -50,26 +50,91 @@ function procesarPreguntas(event){
     event.preventDefault();
 
     const usuario = document.getElementById('usuario').value;
-    const dia  = document.getElementById('diaNacimiento').value;
-    const mes = document.getElementById('mesNacimiento').value;
-    const ano = document.getElementById('anoNacimiento').value;
+    const dia  = Number(document.getElementById('diaNacimiento').value);
+    const mes = Number(document.getElementById('mesNacimiento').value);
+    const ano = Number(document.getElementById('anoNacimiento').value);
 
     let caracteres=usuario.length;
+    let primera;
+     for(let i=0;i<usuario.length;i++){
+            if(usuario[i]==='A'||usuario[i]==='a'){primera=i+1;break;} 
+     }
+
+     let ultima;
+     for(let z=usuario.length-1;z>=0;z--){
+                   if(usuario[z]==='A'|| usuario[z]==='a'){ ultima=z+1;break;} 
+
+     }
+
+    let menos3=usuario.split('');
+    let cortado=menos3.splice(0,3);
+    let res=cortado.join('');
+
+    let mayusculas=usuario.toUpperCase();
+
+    function edad(dia,mes,ano){
+        let hoy = new Date();
+        let nacimiento = new Date(ano, mes - 1, dia);       
+        let años = hoy.getFullYear() - nacimiento.getFullYear();
+
+        if (hoy.getMonth() > nacimiento.getMonth()) { // hoy > nacimiento
+   
+            return años;
+
+        } else if (hoy.getMonth() === nacimiento.getMonth() && hoy.getDate() >= nacimiento.getDate()) {
+            // si es el mismo mes y el dia es mayor o igual que el de nacimiento
+            return años;
+        } else {
+            //aun no he llegado el cumpleaños quito 1 
+            return años - 1;
+        }
+   
+   
+    }
+
+  
+        let nacimiento1 = new Date(ano, mes - 1, dia);       
+        
+        let angulo = 180; // en grados
+        let radianes = angulo * Math.PI / 180; // convertir a radianes
+        let coseno = Math.cos(radianes);
+       
+
+    
+   
+   
+    let salidaEdad=edad(dia,mes,ano);
 
 
-    ///Datos procesados
+    let numeros=[34,67,23,75,35,19];
+
+    let mayor=Math.max(...numeros);
+
+    let azar= Math.random();
+
+    
+
+    
+
+
+
+
+
+    ///Datos procesados Alvaro
 
     const datos=`<h2><Buenos días ${usuario}</h2>
-                    <h2>Tu nombre tiene ${caracteres} caracteres, incluidos espacios.</h2>
-                    <h2>La primera letra A de tu nombre está en la posición:</h2>
-                    <h2>La última letra A de tu nombre está en la posición: <X></h2>
-                    <h2>Tu nombre menos las 3 primeras letras es: <XXXXXXXX></h2>
-                    <h2>Tu nombre todo en mayúsculas es: <XXXXXXXX></h2>
-                    <h2>Tu edad es: <XX> años.</h2>
-                    <h2>Naciste un feliz <XXXX> del año <XXXX>.</h2>
-                    <h2>El coseno de 180 es: <XXXXXXXX></h2>
-                    <h2>El número mayor de (34,67,23,75,35,19) es: <XX></h2>
-                    <h2>Ejemplo de número al azar: <XXXXXXXX></h2>;`
+                    <h4>Tu nombre tiene ${caracteres} caracteres, incluidos espacios.</h4>
+                    <h4>La primera letra A de tu nombre está en la posición: ${primera}</h4> 
+                    <h4>La última letra A de tu nombre está en la posición: ${ultima}</h4>
+                    <h4>Tu nombre menos las 3 primeras letras es: ${res}</h4>
+                    <h4>Tu nombre todo en mayúsculas es: ${mayusculas} </42>
+                    <h4>Tu edad es: ${salidaEdad} años.</h4>
+                    <h4>Naciste un feliz ${nacimiento1.getDay()} del año ${nacimiento1.getFullYear()} .</h4>
+                    <h4>El coseno de 180 es: ${coseno}</h4>
+                    <h4>El número mayor de (34,67,23,75,35,19) es: ${mayor}</42>
+                    <h4>Ejemplo de número al azar: ${azar} </h4>
+                    <p style="text-align: right; font-size: 0.5em; color: #555;">Valencia ❤️ Made in love 14-11-2025</p>`
+                    
 
    let salida=document.getElementById('datos');
 
