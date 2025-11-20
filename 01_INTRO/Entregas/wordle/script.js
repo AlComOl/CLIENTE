78513177e6145d0501de0e0c040a7f60e70fdfa2
Alvaro <alvcomoli@alu.edu.gva.es>
2025-11-20 22:51:41 +0100

    wordle 1:24 queda tela

--- 01_INTRO/Entregas/wordle/script.js
+++ 01_INTRO/Entregas/wordle/script.js
@@ -1,13 +1,20 @@
-
+let salidaGanador = document.querySelector('.result')
 let palabra= "index";
-let palabraArray=palabra.split('');
+let palabraArray=palabra.toUpperCase().split('');
 
 
 let filaActual=document.querySelector('.row');
 
-palabraArray.forEach(() => {
-   filaActual.innerHTML += `<input type="text" maxlength="1" class="cuadrado"></input>`
-});
+palabraArray.forEach((item,index) => {//item representa los elementos del array 
+    if(index===0){
+        filaActual.innerHTML += `<input type="text" maxlength="1" class="cuadrado focus"></input>`
+    }else{
+        filaActual.innerHTML += `<input type="text" maxlength="1" class="cuadrado"></input>`
+    }
+    });
+let elementoFocus = document.querySelector('.focus');
+elementoFocus.focus();//Al recargar la pagina el cursor estara en el 1 recuadro
+
 
 let cuadrados=document.querySelectorAll('.cuadrado');
 cuadrados=[...cuadrados];//convierte array el nodeList asi tengo mas funciones para utilizar (foreach etc..)
@@ -16,8 +23,47 @@ let entradaUsuario=[];
 
 cuadrados.forEach(element=>{
     element.addEventListener('input',event=>{
-        console.log(event.target.value);
+        entradaUsuario.push(event.target.value.toUpperCase());
+        console.log(entradaUsuario);//recojo el array las letras del wordle
+       if(event.target.nextElementSibling){ // envento->target->nextElementSibling(hace señale sig input(cuadrado))Si existe next
+            event.target.nextElementSibling.focus()//focus ,pasa al siguinete
+       }else{
+            //comparamos los arrays para cambiar estilos(verde, amarillo)
+            let indicesCorrectos = compararPalabras(palabraArray,entradaUsuario)
+            indicesCorrectos.forEach(element =>{
+                cuadrados[element].classList.add('green');//cada elemento del array cuadrados le añade la clase green
+            });
+            if(indicesCorrectos.length == palabraArray.length){
+                salidaGanador.innerHTML=`<h1 style='color:red'>GANASTE</h1>
+                <button class="boton">Reiniciar</button>`
+            }
+
+
+            //añadimos nuevas lineas
+       
+          
+                
+
+      
+        }
         
     });
 })
+
+
+///FUNCIONES///
+
+function compararPalabras(array1,array2){
+    indicesIguales=[];
+    array1.forEach((element,index)=>{
+        if(element == array2[index]){
+            console.log("La posicion ${index} SON iguales")
+            indicesIguales.push(index);
+        }else{
+            console.log("La posicion ${index} NO son iguales") 
+        }
+    });
+
+return indicesIguales;
+}
 
