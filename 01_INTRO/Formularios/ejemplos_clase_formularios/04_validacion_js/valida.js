924ba4ad93dd8934ef9c3bb6059b95ef5ec869e7
Alvaro <alvcomoli@alu.edu.gva.es>
2025-12-06 07:09:48 +0100

    formularios

---
+++ 01_INTRO/Formularios/ejemplos_clase_formularios/04_validacion_js/valida.js
@@ -0,0 +1,26 @@
+// function manipulateValues() {
+//   // Obtener el valor del campo de texto
+//   let textInput = document.getElementById("textInput");
+//   console.log("Valor del campo de texto:", textInput.value);
+//   textInput.value = "Nuevo texto";
+
+//   // Obtener el valor del radio button seleccionado
+//   let selectedOption = document.querySelector('input[name="options"]:checked');
+//   console.log("Valor del radio button seleccionado:", selectedOption.value);
+//   // Cambiar la selección del radio button
+//   document.querySelector(
+//     'input[name="options"][value="opcion2"]'
+//   ).checked = true;
+
+//   // Obtener el valor del checkbox
+//   let checkboxInput = document.getElementById("checkboxInput");
+//   console.log("Checkbox está marcado:", checkboxInput.checked);
+//   // Cambiar el estado del checkbox
+//   checkboxInput.checked = !checkboxInput.checked;
+
+//   // Obtener el valor del select
+//   let selectInput = document.getElementById("selectInput");
+//   console.log("Valor del select:", selectInput.value);
+//   // Cambiar la selección del select
+//   selectInput.value = "3";
+// }
