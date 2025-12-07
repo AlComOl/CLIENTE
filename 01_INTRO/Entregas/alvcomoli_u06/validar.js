f56a5235f05f6db377ed97813d748db389d9928e
Alvaro <alvcomoli@alu.edu.gva.es>
2025-12-07 01:19:03 +0100

    form

--- 01_INTRO/Entregas/alvcomoli_u06/validar.js
+++ 01_INTRO/Entregas/alvcomoli_u06/validar.js
@@ -1,14 +1,60 @@
-function validar(){
-    const name=document.getElementById('nombre');
-    const nameRGEX=/^[a-zA-ZÁÉÍÓÚáéíóúñü]+/
-    const validarNombre= nameRGEX.test(name);
+// function validarNombre(){
+//     const name=document.getElementById('nombre');
+//     const nameRGEX=/^[a-zA-ZÁÉÍÓÚáéíóúñü]+/
+//     const validarNombre= nameRGEX.test(name);
 
-    if(validarNombre==false){
+//     if(validarNombre==false){
+//         let msgeror=document.getElementById('error');
+//         msgeror.classList.add('error');
+
+//         msgeror.innerHTML='<strong>El nombre contiene caracteres invalidos<strong>';
+        
+//     }  
+//     return false;
+// }
+
+document.addEventListener('submit',(event)=>{
+
+
+    //valida nombre
+     const name=document.getElementById('nombre').value;//la cagava con el value
+     const nameRGEX=/^[a-zA-ZÁÉÍÓÚáéíóúñü]+$/
+     const validarNombre= nameRGEX.test(name);
+
+      if(validarNombre==false){
+        event.preventDefault();
+
         let msgeror=document.getElementById('error');
         msgeror.classList.add('error');
+        msgeror.innerHTML='<strong>El nombre contiene caracteres invalidos<strong>';    
+    }
 
-        msgeror.innerHTML='<strong>El nombre contiene caracteres invalidos<strong>';
-        
-    }  
-    return false;
-}
+    //valida apellido1
+     const apellido1=document.getElementById('1Apellido').value;
+     const RGEXApellido1=/^[a-zA-ZÁÉÍÓÚáéíóúñü]+$/ //la $ filtra hasta el final si no la pones y pones digitos se lo traga
+     const validarApellido1= RGEXApellido1.test(apellido1);
+
+      if(validarApellido1==false){
+        event.preventDefault();
+
+        let msgeror=document.getElementById('error1');
+        msgeror.classList.add('error');
+        msgeror.innerHTML='<strong>El 1º Apellido contiene caracteres invalidos<strong>';    
+    }
+   //valida apellido2
+
+     const apellido2=document.getElementById('2Apellido').value;
+     const RGEXApellido2=/^[a-zA-ZÁÉÍÓÚáéíóúñü]+$/
+     const validarApellido2= RGEXApellido2.test(apellido2);
+
+      if(validarApellido2==false){
+        event.preventDefault();
+
+        let msgeror=document.getElementById('error2');
+        msgeror.classList.add('error');
+        msgeror.innerHTML='<strong>El 2º Apellido contiene caracteres invalidos<strong>'; 
+
+    
+      }
+      
+});
