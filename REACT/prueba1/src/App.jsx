e85e6e5639dff0e29519d53e8a96d19ad021916e
AlComOl <alvcomoli@alu.edu.gva.es>
2026-01-09 09:55:27 +0100

    ejercicios react de nuevo

--- REACT/prueba1/src/App.jsx
+++ REACT/prueba1/src/App.jsx
@@ -1,26 +1,96 @@
+// const App = () => {
+//   const course = 'Half Stack application development'
+//   const part1 = {
+//     name: 'Fundamentals of React',
+//     exercises: 10
+//   }
+//   const part2 = {
+//     name: 'Using props to pass data',
+//     exercises: 7
+//   }
+//   const part3 = {
+//     name: 'State of a component',
+//     exercises: 14
+//   }
 
-const Hijo = (props) =>{
+//   return (
+//     <div>
+//     <h1>{course}</h1>
+//     <p>{part1.name} {part1.exercises}</p>
+//     <p>{part2.name} {part2.exercises}</p>
+//     <p>{part3.name} {part3.exercises}</p>
+//     </div>
+//   )
+// }
+
+
+//1.4
+
+
+const Header = (props) =>{
+  
   return(
-    <p>Soy el hijo de APP {props.name} {props.apellido}</p>
-    
+  <h1>{props.course}</h1>
   )
 }
 
+
+
+const Content = (props) =>{
+  return(
+    <ul>
+      <li>{props.parts.part1.name} {props.parts.part1.exercises}</li>
+      <li>{props.parts.part2.name} {props.parts.part2.exercises}</li>
+      <li>{props.parts.part3.name} {props.parts.part3.exercises}</li>
+    </ul>
+  
+  );
+
+}
+
+const Total = (props) => {
+  const total= props.parts.part1.exercises+props.parts.part2.exercises+props.parts.part3.exercises;
+  return(
+
+  <ul>
+    <li>Total exercices {total}</li>
+  </ul>
+  )
+}
+ 
+
+
 const App = () => {
-const  name1 = 'Luis'
- const name2='Jose'
- const name3 = 'Pepe'
+  const course = 'Half Stack application development'
+
+  const parts = {
+
+    part1:{
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    part2:{
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    part3:{
+      name: 'State of a component',
+      exercises: 14
+    }
+  }
 
   return (
     <div>
-      <p>Hello world</p>
-      < Hijo  name='Alvaro'/>
-      < Hijo  name={name2} apellido='Amoros'/>
-      < Hijo  name={name3} apellido='Amoros'/>
+      <Header course={course}/>
+      <Content parts ={parts}/>
+      <Total parts={parts}/>
+      
     </div>
-    
   )
 }
+
+
+
 
 
 export default App
