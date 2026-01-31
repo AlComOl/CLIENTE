76ead65992874432995547b26ba73b500df52413
Alvaro <alvcomoli@alu.edu.gva.es>
2026-02-01 00:10:08 +0100

    cards

--- FrontAG/src/components/dashboard.jsx
+++ FrontAG/src/components/dashboard.jsx
@@ -1,66 +1,100 @@
 import './StyleComponents/ComponetsNavStyle.css';
 
-const Card = ({texto,iconImg,altText,valor,comentario}) =>{
-  return(
-
+const Card = ({ texto, iconImg, altText, valor, comentario, children }) => {
+  return (
     <div className="card1">
       <div className="texto">
         <p>{texto}</p>
         <img src={iconImg} alt={altText} />
       </div>
-        <h5 className="value">{valor}</h5>
-          <div>
-            <p className ="texto">{comentario}</p>
-          </div>
-    </div>
 
-  )
-}
-const dashboard = () =>{
-return(
-     <div>
+      <h5 className="value">{valor}</h5>
+
+      <div>
+        <p className="texto">{comentario}</p>
+      </div>
+
+      {/* Aquí se renderiza cualquier componente hijo */}
+      {children}
+    </div>
+  );
+};
+
+const ShortCard = ({ titulo, texto }) => {
+  return (
+    <div className="cardShort">
+      <h6>{titulo}</h6>
+      <p>{texto}</p>
+      <div>
+        <button>Ver</button>
+      </div>
+    </div>
+  );
+};
+
+const Dashboard = () => {
+  return (
+    <div>
       <p>Resumen general de la gestión agrícola</p>
 
-      <div className='primeraSeccion' >
+      <div className="primeraSeccion">
         <Card
-        iconImg="./iconExplotacion.svg"
-        altText="Explotaciones"
-        texto="Explotaciones"
-        valor="9"
-        comentario="Total de Fincas"/>
+          iconImg="./iconExplotacion.svg"
+          altText="Explotaciones"
+          texto="Explotaciones"
+          valor="9"
+          comentario="Total de Fincas"
+        />
 
-         <Card
-        iconImg="./iconParcelas.svg"
-        altText="parcelas"
-        texto="Parcelas"
-        valor="35"
-        comentario="Total de Fincas"/>
+        <Card
+          iconImg="./iconParcelas.svg"
+          altText="parcelas"
+          texto="Parcelas"
+          valor="35"
+          comentario="Total de Fincas"
+        />
 
-         <Card
-        iconImg="./iconOperaciones.svg"
-        altText="operaciones"
-        texto="Operaciones"
-        valor="34"
-        comentario="Total Operaciones"/>
+        <Card
+          iconImg="./iconOperaciones.svg"
+          altText="operaciones"
+          texto="Operaciones"
+          valor="34"
+          comentario="Total Operaciones"
+        />
 
-         <Card
-        iconImg="./iconAlmacen.svg"
-        altText="Almacen"
-        texto="Productos"
-        valor="9"
-        comentario="En almacén"/>
-
-        
+        <Card
+          iconImg="./iconAlmacen.svg"
+          altText="Almacen"
+          texto="Productos"
+          valor="9"
+          comentario="En almacén"
+        />
       </div>
-      
 
+      <div className="segundaSeccion">
+     
+        <Card
+          iconImg="./iconAlmacen.svg"
+          altText="Alertas"
+          texto="Alertas"
+          valor="9"
+          comentario="Requiere atención"
+        >
+          <ShortCard titulo="Nueva alerta" texto="Revisar fincas" />
+        </Card>
 
-
+        <Card
+          iconImg="./iconAlmacen.svg"
+          altText="Almacen"
+          texto="Productos"
+          valor="9"
+          comentario="En almacén"
+        >
+          <ShortCard titulo="Nueva alerta" texto="Revisar fincas" />
+        </Card>
+      </div>
     </div>
+  );
+};
 
-)
-}
-
-
-
-export default dashboard 
+export default Dashboard;
--- FrontAG/src/components/menuNav.jsx
+++ FrontAG/src/components/menuNav.jsx
@@ -1,12 +1,14 @@
 import { Link } from 'react-router-dom';
+import {useState} from 'react';
 import './menuNav.css';
 
 
-const BtnNav =({to,texto,iconImg,altText}) =>{
+const BtnNav =({to,texto,iconImg,altText,isSeleccionado, onClick}) =>{
+
 
   return(
-  <Link to={to} className="router-link">
-  <div className='logo'>
+  <Link to={to} className="router-link" onClick={onClick}>
+  <div className={`logo ${isSeleccionado ? 'activeMenu' : ''}`} onClick={onClick}>
       <a className='texto'>
         <img className='iconMenu' src={iconImg} alt={altText} />
         <span>{texto}</span>
@@ -21,46 +23,59 @@ const BtnNav =({to,texto,iconImg,altText}) =>{
 
 //meto componentes dentro de la barraNav
 const BarraMenu = () => {
+
+  const [botonActivo, setBotonActivo] = useState('Dashboard');
   return(
 
 <div className="navbar">
 
   <BtnNav  to="./dashboard"
-     iconImg="./iconDashboard.svg"
+     iconImg ="./iconDashboard.svg"
      altText="Dashboard"
-     texto="Dashboard"/>
+     texto="Dashboard"
+     isSeleccionado={botonActivo === 'Dashboard'} 
+     onClick={() => setBotonActivo('Dashboard')}/>
 
   <BtnNav
     to="/explotaciones"
     iconImg="./iconExplotacion.svg"
     altText="Explotaciones"
     texto="Explotaciones" 
-    /> 
+    isSeleccionado={botonActivo === 'Explotaciones'} 
+    onClick={() => setBotonActivo('Explotaciones')}/>
 
   <BtnNav
     to="./parcelas"
     iconImg="./iconParcelas.svg"
     altText="Parcelas"
-    texto="Parcelas" />
+    texto="Parcelas" 
+    isSeleccionado={botonActivo === 'Parcelas'} 
+    onClick={() => setBotonActivo('Parcelas')}/>
 
      
   <BtnNav 
      to="./operaciones"
      iconImg="./iconOperaciones.svg"
      altText="Parcelas"
-     texto="Operaciones"/>
+     texto="Operaciones"
+     isSeleccionado={botonActivo === 'Operaciones'} 
+    onClick={() => setBotonActivo('Operaciones')}/>
 
   <BtnNav 
      to="./recoleccion"
      iconImg="./iconRecoleccion.svg"
      altText="Recoleccion"
-     texto="Recoleccion"/>
+     texto="Recoleccion"
+     isSeleccionado={botonActivo === 'Recoleccion'} 
+    onClick={() => setBotonActivo('Recoleccion')}/>
 
   <BtnNav 
      to="./almacen"
      iconImg="./iconAlmacen.svg"
      altText="Almacen"
-     texto="Almacen"/>
+     texto="Almacen"
+     isSeleccionado={botonActivo === 'Almacen'} 
+     onClick={() => setBotonActivo('Almacen')}/>
     
 </div>
   )
